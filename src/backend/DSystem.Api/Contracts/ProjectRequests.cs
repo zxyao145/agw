ce5d3dc96448fef6f4a6fcb43f4778da15512a40
zxyao <zxyao145@gmail.com>
2026-01-28 13:35:42 +0800

    feat: add project extra settings (#14)

--- src/backend/DSystem.Api/Contracts/ProjectRequests.cs
+++ src/backend/DSystem.Api/Contracts/ProjectRequests.cs
@@ -1,5 +1,5 @@
 namespace DSystem.Api.Contracts;
 
-public record ProjectCreateRequest(string Name, string? Description, bool Enable);
+public record ProjectCreateRequest(string Name, string? Description, bool Enable, string? ExtraSetting);
 
-public record ProjectUpdateRequest(string Name, string? Description, bool Enable);
+public record ProjectUpdateRequest(string Name, string? Description, bool Enable, string? ExtraSetting);
